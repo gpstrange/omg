e25d34494de9be143a20f0122d47fd9406331c76
Guru Prasad G <prasadguru141@gmail.com>
2019-09-01 09:42:26 +0530

    Fixed auth screen

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,24 +1,42 @@
 <template>
   <div id="add-feed">
     <h3 align="center">Welcome to the world of Gossips!</h3>
-    <form>
-        <md-field>
-            <label style="font-size: 12px">Your Secret Name</label>
-            <md-input v-model="username"></md-input>
-        </md-field>
-        <md-field>
-            <label style="font-size: 12px">Password</label>
-            <md-input v-model="password" type="password"></md-input>
-        </md-field>
-        <div align="center">
-            <router-link to="/login">Already have account - Login</router-link>
-        </div>
-        <div align="center">
-            <md-button v-on:click="onSubmit" class="md-accent md-raised" style="margin-top: 10px">
-                SignUp
-            </md-button>
-        </div>
-    </form>
+    <md-tabs class="md-transparent" md-alignment="fixed">
+      <md-tab id="tab-login" md-label="LOGIN">
+        <form>
+          <md-field>
+              <label style="font-size: 12px">Your Secret Name</label>
+              <md-input v-model="username"></md-input>
+          </md-field>
+          <md-field>
+              <label style="font-size: 12px">Password</label>
+              <md-input v-model="password" type="password"></md-input>
+          </md-field>
+          <div align="center">
+              <md-button v-on:click="onLogin" class="md-accent md-raised" style="margin-top: 10px">
+                  Login
+              </md-button>
+          </div>
+        </form>
+      </md-tab>
+      <md-tab id="tab-signup" md-label="SIGN UP">
+        <form>
+          <md-field>
+              <label style="font-size: 12px">Your Secret Name</label>
+              <md-input v-model="username"></md-input>
+          </md-field>
+          <md-field>
+              <label style="font-size: 12px">Password</label>
+              <md-input v-model="password" type="password"></md-input>
+          </md-field>
+          <div align="center">
+              <md-button v-on:click="onSubmit" class="md-accent md-raised" style="margin-top: 10px">
+                  SignUp
+              </md-button>
+          </div>
+        </form>
+      </md-tab>
+    </md-tabs>
     <div>
       <md-snackbar md-position="center" :md-duration="3000" :md-active.sync="showSnackbar" md-persistent>
       <span>{{errMessage}}</span>
@@ -62,6 +80,34 @@ export default {
         localStorage.setItem('token', res.data.token)
         localStorage.setItem('user', JSON.stringify(res.data.user))
         this.$router.push('college')
+      }).catch((err) => {
+        if (err.response && err.response.data && err.response.data.message) {
+          this.errMessage = err.response.data.message
+        } else {
+          this.errMessage = 'Something went wrong'
+        }
+        this.showSnackbar = true
+      })
+    },
+    onLogin (event) {
+      if (!this.username || !this.password) {
+        return
+      }
+      const payload = {
+        name: this.username,
+        password: this.password
+      }
+
+      axios.post(URL + '/login', payload).then((res) => {
+        // payload.groupId = res.data._id
+        localStorage.setItem('token', res.data.token)
+        localStorage.setItem('user', JSON.stringify(res.data.user))
+        if (res.data.user.groupId) {
+          localStorage.setItem('groupId', res.data.user.groupId)
+          this.$router.push('home')
+        } else {
+          this.$router.push('college')
+        }
       }).catch((err) => {
         if (err.response && err.response.data && err.response.data.message) {
           this.errMessage = err.response.data.message
--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import AddFeed from '@/components/Add-Feed'
 import SelectCollege from '@/components/Select-College'
 import AddCollege from '@/components/Add-College'
 import Auth from '@/components/Auth'
-import Login from '@/components/Login'
 import Comment from '@/components/Comment'
 
 Vue.use(Router)
@@ -14,13 +13,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Signup',
+      name: 'Auth',
       component: Auth
     },
     {
       path: '/login',
       name: 'Login',
-      component: Login
+      component: Auth
     },
     {
       path: '/college',
